6d987050dafd249e58a20f7b4765b92fc6f3613a
chuccp <cooge123@gmail.com>
2022-02-18 14:57:59 +0800

    Update QueryUtils.java

--- src/main/java/com/kanke/search/query/QueryUtils.java
+++ src/main/java/com/kanke/search/query/QueryUtils.java
@@ -1,14 +1,17 @@
 package com.kanke.search.query;
 
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
 
 import org.apache.lucene.document.NumericDocValuesField;
 import org.apache.lucene.index.Term;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.BooleanQuery.Builder;
 import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermInSetQuery;
 import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.WildcardQuery;
+import org.apache.lucene.util.BytesRef;
 
 public class QueryUtils {
 
@@ -26,6 +29,20 @@ public class QueryUtils {
 		return query;
 	}
 
+	public static Query createTermsQuery(String field, Collection<String> valueList) {
+		return createTermsQuery(field, valueList.toArray(new String[] {}));
+
+	}
+
+	public static Query createTermsQuery(String field, String... values) {
+		List<BytesRef> list = new ArrayList<>();
+		for (String value : values) {
+			list.add(new BytesRef(value));
+		}
+		TermInSetQuery termInSetQuery = new TermInSetQuery(field, list);
+		return termInSetQuery;
+	}
+
 	public static Query createQuery(String field, boolean value) {
 		int v = value ? 1 : 0;
 		return NumericDocValuesField.newSlowRangeQuery(field, v, v);
@@ -38,8 +55,7 @@ public class QueryUtils {
 	public static Query createRangeQuery(String field, Date lowerValue, Date upperValue) {
 		return NumericDocValuesField.newSlowRangeQuery(field, lowerValue.getTime(), upperValue.getTime());
 	}
-	
-	
+
 	public static BuilderQuery createBoolQuery() {
 		return new BuilderQuery();
 	}
